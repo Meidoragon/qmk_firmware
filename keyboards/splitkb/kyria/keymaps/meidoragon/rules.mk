99d4e4adf118f5d2b498e0816748b5e8a8b3b6e5
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-06-05 14:33:37 -0500

    add my map and remove all boards i don't own

---
+++ keyboards/splitkb/kyria/keymaps/meidoragon/rules.mk
@@ -0,0 +1,8 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CONVERT_TO = liatris 		   # Convert from Elite-C to Liatris
+# LTO_ENABLE = yes
+# MUSIC_ENABLE = no
+
